a063739ed023cb0e767e5f2bcbbc2a0b4db6c6c4
何奕瑋 <david.ho@program.com.tw>
2023-07-11 15:26:07 +0800

    refactor: 修改關聯資料獲取方式 #5

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\NewsRequest;
 use App\Models\Area;
 use App\Models\Menu;
 use App\Models\News;
@@ -15,25 +16,18 @@ class NewsController extends Controller
     public function index()
     {
         $collection = News::all();
-        // query menu id to menu name
+
+        // add $collection[]->area and $collection[]->menu to $collection
         foreach ($collection as $key => $value) {
-            $menu = Menu::find($value['menu']);
-            $collection[$key]['menu'] = $menu['name'];
+            $collection[$key]['area'] = $value->area();
+            $collection[$key]['menu'] = $value->menu();
         }
-        // query area id to area name
-        foreach ($collection as $key => $value) {
-            $area = [];
-            foreach ($value['area'] as $areaId) {
-                $areaName = Area::find($areaId);
-                array_push($area, $areaName['name']);
-            }
-            $collection[$key]['area'] = $area;
-        }
+
         return response()->json($collection);
     }
 
     // add new news
-    public function store(Request $request)
+    public function store(NewsRequest $request)
     {
         $areaString = $request->input('area');
         if ($areaString == '') {
@@ -49,29 +43,15 @@ class NewsController extends Controller
         $content = $request->input('content');
 
         try {
-            $validated = $request->validate([
-                'area' => ['required'],
-                'menu' => ['required'],
-                'start_at' => ['required', 'date'],
-                'end_at' => ['required', 'date', 'after:start_at'],
-                'status' => ['required', 'numeric', Rule::in([0, 1])],
-                'title' => ['required'],
-                'content' => ['required'],
-            ], [
-                'area.required' => '請填入區域',
-                'menu.required' => '請填入選單',
-                'start_at.required' => '請填入開始時間',
-                'start_at.date' => '開始時間必須是一個有效日期',
-                'end_at.required' => '請填入結束時間',
-                'end_at.date' => '結束時間必須是一個有效日期',
-                'end_at.after' => '結束時間必須大於開始時間',
-                'status.required' => '請填入狀態',
-                'status.numeric' => '狀態必須是顯示或隱藏',
-                'status.in' => '狀態必須是顯示或隱藏',
-                'title.required' => '請填入標題',
-                'content.required' => '請填入內容',
+            News::create([
+                'area_id' => $area,
+                'menu_id' => $menu,
+                'start_at' => $start_at,
+                'end_at' => $end_at,
+                'status' => $status,
+                'title' => $title,
+                'content' => $content
             ]);
-            News::create(['area' => $area, 'menu' => $menu, 'start_at' => $start_at, 'end_at' => $end_at, 'status' => $status, 'title' => $title, 'content' => $content]);
             return response()->json(['status' => 'success', 'message' => 'News added successfully!']);
         } catch (ValidationException $e) {
             $errors = $e->validator->errors()->all();
@@ -83,18 +63,13 @@ class NewsController extends Controller
     public function show($id)
     {
         $collection = News::find($id);
-        // query area id to area name save id and name
-        $area = [];
-        foreach ($collection['area'] as $areaId) {
-            $areaName = Area::find($areaId);
-            array_push($area, ['id' => $areaId, 'name' => $areaName['name']]);
-        }
-        $collection['area'] = $area;
+        $collection['area'] = $collection->area();
+        $collection['menu'] = $collection->menu();
         return response()->json($collection);
     }
 
     // edit a news by id
-    public function update(Request $request, $id)
+    public function update(NewsRequest $request, $id)
     {
         $areaString = $request->input('area');
         if ($areaString == '') {
@@ -109,26 +84,16 @@ class NewsController extends Controller
         $title = $request->input('title');
         $content = $request->input('content');
 
-        if (
-            empty($area) ||
-            empty($menu) ||
-            empty($start_at) ||
-            empty($end_at) ||
-            ($status != 0 && $status != 1) ||
-            empty($title) ||
-            empty($content)
-        ) {
-            return response()->json(['status' => 'error', 'message' => '請填入所有欄位'], 422);
-        }
-
         try {
-            $validated = $request->validate([
-                'start_at' => ['required', 'date'],
-                'end_at' => ['required', 'date', 'after:start_at'],
-            ], [
-                'end_at.after' => '結束時間必須大於開始時間。',
+            News::where('_id', $id)->update([
+                'area_id' => $area,
+                'menu_id' => $menu,
+                'start_at' => $start_at,
+                'end_at' => $end_at,
+                'status' => $status,
+                'title' => $title,
+                'content' => $content
             ]);
-            News::where('_id', $id)->update(['area' => $area, 'menu' => $menu, 'start_at' => $start_at, 'end_at' => $end_at, 'status' => $status, 'title' => $title, 'content' => $content]);
             return response()->json(['status' => 'success', 'message' => 'News updated successfully!']);
         } catch (ValidationException $e) {
             $errors = $e->validator->errors()->all();
--- app/Models/Area.php
+++ app/Models/Area.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 
 class Area extends Model
 {
-    use HasFactory;
 
     protected $collection = 'area';
     protected $connection = 'mongodb';
